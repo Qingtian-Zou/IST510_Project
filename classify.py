931d7aafc065acef84cef66ed5761d2be1946d63
Qingtian <qingtian.mill.zou@gmail.com>
2018-02-16 10:52:04 -0500

    Done "get_images.py"

---
+++ classify.py
@@ -0,0 +1,9 @@
+import argparse
+
+def main():
+    pass
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser()
+    FLAGS, unparsed= parser.parse_known_args()
+    main()
---
+++ get_images.py
@@ -0,0 +1,42 @@
+import requests
+import argparse
+import os
+
+FLAGS=None
+
+def main():
+    headers={'X-Requested-With': 'XMLHttpRequest'}
+    url='https://cors-anywhere.herokuapp.com/https://www.google.com.ua/search?source=lnms&sa=X&gbv=1&tbm=isch&q='+FLAGS.keyword
+    h=requests.get(url,headers=headers)
+    xml_file=open('results.html', 'w+')
+    xml_file.write(h.text)
+    xml_file.close()
+    lines=h.text.split()
+    images_url=[]
+    for line in lines:
+        if line[0:13]=='src="https://':
+            images_url.append(line.split('=',1)[1].replace('"',''))
+    url_file=open('image_url.txt','w+')
+    url_file.writelines(images_url)
+    url_file.close()
+    i=0
+    for url in images_url:
+        img_data=requests.get(url).content
+        if not os.path.exists(FLAGS.keyword):
+            os.makedirs(FLAGS.keyword)
+        with open(os.path.join(os.getcwd(),FLAGS.keyword,FLAGS.keyword+str(i+1)+'.jpg'),'wb') as handler:
+            handler.write(img_data)
+        i+=1
+        if i==20:
+            break
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser()
+    parser.add_argument(
+        '--keyword',
+        type=str,
+        default='',
+        help='keyword for image searching'
+    )
+    FLAGS, unparsed= parser.parse_known_args() # keyword is not parsed!!!
+    main()
